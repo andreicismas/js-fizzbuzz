5f587271ebac6393a48b43b0cc97d2fbd86ac82f
andreicismas <andrei_cismas@yahoo.it>
2021-04-12 18:05:16 +0200

    fin project added && to if

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // stampo i numeri fino a 100 multipli di "3" "5" e "15"
 for (var i = 0; i < 100; i++) {  
      //stampa il multiplo di 3 e 5 =15
-    if (i % 15 === 0)
-        console.log(i+" "+"io sono FizzBuzz" + " " + "%15");
+    if (i % 5 !== 0 && i % 3 !== 0)
+        console.log(i+" "+"io sono FizzBuzz");
         //  //stampa il multiplo di 3 
     else if (i % 3 === 0)
         console.log(i+" "+"io sono Fizz" + " " + "%3");
